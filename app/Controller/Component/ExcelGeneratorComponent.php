5df24aef8ef69243b878fe16301399f7a7c15d10
Guilherme Gralho <guilherme.gralho@connectcom.com.br>
2023-09-22 11:27:10 -0300

    Corrige excel outcome

--- app/Controller/Component/ExcelGeneratorComponent.php
+++ app/Controller/Component/ExcelGeneratorComponent.php
@@ -99,4 +99,16 @@ class ExcelGeneratorComponent extends Component
         $writer = new Xlsx($spreadsheet);
         $writer->save($local_salva);
     }
+
+    public function gerarExcelOutcome($nome, $dados)
+    {
+        $spreadsheet = new Spreadsheet();
+
+        $this->templates_list->getOutcome($spreadsheet, $dados);
+
+        $local_salva = APP.'webroot/files/excel/'.$nome;
+
+        $writer = new Xlsx($spreadsheet);
+        $writer->save($local_salva);
+    }
 }
