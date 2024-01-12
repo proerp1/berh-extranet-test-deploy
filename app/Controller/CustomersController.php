427ec23eaa6380f350d5c3de4bf47eafc35f1476
GHRPorcel <gporcel49@gmail.com>
2024-01-12 16:19:24 -0300

    ,

--- app/Controller/CustomersController.php
+++ app/Controller/CustomersController.php
@@ -864,13 +864,7 @@ class CustomersController extends AppController
      **********************/
     public function documents($id)
     {
-        $this->Permission->check(11, 'leitura') ? '' : $this->redirect('/not_allowed');
-        $this->Paginator->settings = ['OrderItem' => [
-            'limit' => 100,
-            'order' => ['Document.created' => 'desc'],
-            
-            ]
-        ];
+      
 
         $condition = ['and' => ['Customer.id' => $id], 'or' => []];
 
