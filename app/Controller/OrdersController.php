06ad01f04d9f1c8d366bda69a4b92ad92056045a
proerp1 <112705205+proerp1@users.noreply.github.com>
2024-04-18 15:02:01 -0300

    Update OrdersController.php

--- app/Controller/OrdersController.php
+++ app/Controller/OrdersController.php
@@ -755,7 +755,7 @@ class OrdersController extends AppController
 
             $existingUser = $this->CustomerUser->find('first', [
                 'conditions' => [
-                    "REPLACE(REPLACE(CustomerUser.cpfa, '-', ''), '.', '')" => $cpf,
+                    "REPLACE(REPLACE(CustomerUser.cpf, '-', ''), '.', '')" => $cpf,
                     'CustomerUser.customer_id' => $customerId,
                 ]
             ]);
