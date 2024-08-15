bbba3790384faafe1133d1886664bb0b1ce8df3c
GHRPorcel <gporcel49@gmail.com>
2024-08-15 18:04:53 -0300

    ajusta memoria e tempo de relatorio

--- app/Controller/CustomerUsersController.php
+++ app/Controller/CustomerUsersController.php
@@ -101,6 +101,10 @@ class CustomerUsersController extends AppController
 
     public function generate_excel_report($id)
     {
+        ini_set('memory_limit', '-1');
+
+	    set_time_limit(90);
+        ini_set('max_execution_time', -1); 
     
         $data = $this->CustomerUser->find('all', [
             'conditions' => ['CustomerUser.customer_id' => $id],
