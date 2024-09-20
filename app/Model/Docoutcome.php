39b6cd9505d50efe91f51310fee8a872e3ec4b9e
Gabriel Porcel <gporcel49@gmail.com>
2024-09-20 15:13:10 -0300

    ajustes em join

--- app/Model/Docoutcome.php
+++ app/Model/Docoutcome.php
@@ -1,67 +1,28 @@
 <?php 
+App::uses('AuthComponent', 'Controller/Component');
 class Docoutcome extends AppModel {
 	public $name = 'Docoutcome';
 
 	public $belongsTo = array(
-
+		'Status' => array(
+			'className' => 'Status',
+			'foreignKey' => 'status_id',
+			'conditions' => array('Status.categoria' => 1)
+		),
 		'Outcome'
 	);
 
 	public function beforeFind($queryData) {
+
 		$queryData['conditions'][] = array('Docoutcome.data_cancel' => '1901-01-01 00:00:00');
+		
 		return $queryData;
 	}
 
-	public function getDocuments($conditions = [], $limit = 50) {
-		$this->unbindModel(['belongsTo' => ['Outcome']]); // Unbind se não for necessário
-
-		$query = $this->find('all', [
-			'conditions' => $conditions,
-			'limit' => $limit,
-			'joins' => [
-				[
-					'table' => 'outcomes',
-					'alias' => 'Outcome',
-					'type' => 'LEFT',
-					'conditions' => ['Docoutcome.outcome_id = Outcome.id']
-				],
-				[
-					'table' => 'suppliers',
-					'alias' => 'Supplier',
-					'type' => 'LEFT',
-					'conditions' => ['Outcome.supplier_id = Supplier.id']
-				],
-				[
-					'table' => 'statuses',
-					'alias' => 'Status',
-					'type' => 'LEFT',
-					'conditions' => ['Docoutcome.status_id = Status.id']
-				],
-				[
-					'table' => 'statuses',
-					'alias' => 'OutcomeStatus',
-					'type' => 'LEFT',
-					'conditions' => ['Outcome.status_id = OutcomeStatus.id']
-				]
-			],
-			'fields' => [
-				'Docoutcome.*', 
-				'Outcome.*', 
-				'Supplier.nome_fantasia', 
-				'Status.*', 
-				'OutcomeStatus.*'
-			],
-			'order' => [
-				'Outcome.id' => 'asc',
-				'Docoutcome.created' => 'asc'
-			]
-		]);
-
-		return $query;
-	}
-
 	public $actsAs = array(
-		'Upload.Upload' => array('file')
+		'Upload.Upload' => array(
+			'file'
+		)
 	);
 
 	public $validate = array(
@@ -78,4 +39,5 @@ class Docoutcome extends AppModel {
 			)
 		)
 	);
+
 }
