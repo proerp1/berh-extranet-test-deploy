125a4bf006bece5b7666545858beef2301dde72b
Matheus Fernandes Freire <ffreirematheus@gmail.com>
2024-10-01 11:13:41 -0300

    Ajusta op set cookie robos

--- app/Controller/ReportsController.php
+++ app/Controller/ReportsController.php
@@ -676,6 +676,7 @@ class ReportsController extends AppController
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url_cookie);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
         
         $resultado = curl_exec($ch);
         curl_close($ch);
