7fc0234a705f4e63af3c443a34ea790ba86a2105
Gabriel Porcel <gporcel49@gmail.com>
2024-10-11 11:48:19 -0300

    sobe pastas js para graficos

---
+++ app/webroot/js/widget/charts/widget-gestao-eficiente.js
@@ -0,0 +1,145 @@
+"use strict";
+
+// Class definition
+var KTChartsWidget5 = function() {
+    // Private methods
+    var initChart = function() {
+        var element = document.getElementById('kt_apexcharts_gestao_eficiente');
+
+        var height = parseInt(KTUtil.css(element, 'height'));
+        var labelColor = KTUtil.getCssVariableValue('--bs-gray-500');
+        var borderColor = KTUtil.getCssVariableValue('--bs-gray-200');
+        var baseColor = KTUtil.getCssVariableValue('--bs-success');
+        var secondaryColor = KTUtil.getCssVariableValue('--bs-primary');
+
+        if (!element) {
+            return;
+        }
+
+        var options = {
+            series: [{
+                name: 'Valor dos Itens',
+                data: total_pedidos
+            }, {
+                name: 'Economia',
+                data: total_desconto
+            }],
+            chart: {
+                fontFamily: 'inherit',
+                type: 'bar',
+                height: height,
+                toolbar: {
+                    show: false
+                }
+            },
+            plotOptions: {
+                bar: {
+                    horizontal: true,
+                    columnWidth: ['30%'],
+                    endingShape: 'rounded'
+                },
+            },
+            legend: {
+                show: false
+            },
+            dataLabels: {
+                enabled: false
+            },
+            stroke: {
+                show: true,
+                width: 2,
+                colors: ['transparent']
+            },
+            xaxis: {
+                categories: [mes_atual],
+                axisBorder: {
+                    show: false,
+                },
+                axisTicks: {
+                    show: false
+                },
+                labels: {
+                    style: {
+                        colors: labelColor,
+                        fontSize: '12px'
+                    }
+                }
+            },
+            yaxis: {
+                labels: {
+                    style: {
+                        colors: labelColor,
+                        fontSize: '12px'
+                    }
+                }
+            },
+            fill: {
+                opacity: 1
+            },
+            states: {
+                normal: {
+                    filter: {
+                        type: 'none',
+                        value: 0
+                    }
+                },
+                hover: {
+                    filter: {
+                        type: 'none',
+                        value: 0
+                    }
+                },
+                active: {
+                    allowMultipleDataPointsSelection: false,
+                    filter: {
+                        type: 'none',
+                        value: 0
+                    }
+                }
+            },
+            tooltip: {
+                style: {
+                    fontSize: '12px'
+                },
+                y: {
+                    formatter: function(val) {
+                        return 'R$' + val
+                    }
+                }
+            },
+            colors: [secondaryColor, baseColor],
+            grid: {
+                borderColor: borderColor,
+                strokeDashArray: 4,
+                yaxis: {
+                    lines: {
+                        show: true
+                    }
+                }
+            }
+        };
+
+        var chart = new ApexCharts(element, options);
+
+        setTimeout(function() {
+            chart.render();
+        }, 200);
+    }
+
+    // Public methods
+    return {
+        init: function() {
+            initChart();
+        }
+    }
+}();
+
+// Webpack support
+if (typeof module !== 'undefined') {
+    module.exports = KTChartsWidget5;
+}
+
+// On document ready
+KTUtil.onDOMContentLoaded(function() {
+    KTChartsWidget5.init();
+});
