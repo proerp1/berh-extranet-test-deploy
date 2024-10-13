242c473a7135ab58c1ee122e15ffad0d1b2fb3f4
proerp1 <112705205+proerp1@users.noreply.github.com>
2024-10-13 12:01:38 -0300

    Update DashboardController.php

--- app/Controller/DashboardController.php
+++ app/Controller/DashboardController.php
@@ -225,7 +225,7 @@ public function getEvolucaoPedidos()
                 "DATE_FORMAT(Order.order_period_from, '%m/%Y') as mes",
             ],
             'conditions' => [
-                'Order.order_period_froms >=' => date('Y-01-01'),
+                'Order.order_period_from >=' => date('Y-01-01'),
                 'Order.order_period_to <=' => date('Y-12-31'),
                 'Order.status_id' => 87,
             ],
