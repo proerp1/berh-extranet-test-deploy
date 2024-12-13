79b785f9c508a8f2d1f2f4a1d8cdbbf28ca09ee2
Gabriel Porcel <gporcel49@gmail.com>
2024-12-13 11:48:56 -0300

    .

--- app/Controller/Component/ExcelTemplate.php
+++ app/Controller/Component/ExcelTemplate.php
@@ -1899,10 +1899,8 @@ class ExcelTemplate
 				->setCellValue('CA'. $indx, $dados[$i]['Order']["primeiro_pedido"] == "N" ? "Não" : "Sim")
 				->setCellValue('CB'. $indx, $dados[$i]['Order']['pedido_complementar'])
 				->setCellValue('CC'. $indx, $dados[$i]['Order']['saldo_transfer_fee'])
-				->setCellValue('CD'. $indx, $dados[$i]['OrderItem']['transfer_fee'] - $dados[$i]['Order']['saldo_transfer_fee'])
-				$objPHPExcel->getActiveSheet()->getStyle('CD' . $indx)
-    ->getNumberFormat()
-    ->setFormatCode('0.00');
+				->setCellValue('CD'. $indx, $dados[$i]['OrderItem']['transfer_fee'] - $dados[$i]['Order']['saldo_transfer_fee']);
+				
 
 		}
 	}
