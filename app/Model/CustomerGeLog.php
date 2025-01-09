193e307010036fb56d749af5bbcf8cfa1e15baad
Gabriel Porcel <gporcel49@gmail.com>
2025-01-09 14:21:24 -0300

    -

--- app/Model/CustomerGeLog.php
+++ app/Model/CustomerGeLog.php
@@ -13,16 +13,13 @@ class CustomerGeLog extends AppModel {
     );
 
     public function beforeFind($queryData) {
-        // Definindo uma condição de filtro, como já estava no código original
         $queryData['conditions'][] = array('CustomerGeLog.data_cancel' => '1901-01-01 00:00:00');
         return $queryData;
     }
 
     public function afterFind($results, $primary = false) {
-        // Verificando se a chave 'CustomerGeLog' e o campo 'created' estão disponíveis
         foreach ($results as $key => $val) {
             if (isset($val['CustomerGeLog']['created'])) {
-                // Formatar a data para o formato brasileiro (dd/mm/yyyy)
                 $results[$key]['CustomerGeLog']['created'] = (new DateTime($val['CustomerGeLog']['created']))->format('d/m/Y H:i:s');
             }
         }
