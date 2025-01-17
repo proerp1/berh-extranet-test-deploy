783bae75827922fd6d194b817c4db9e218669306
Gabriel Porcel <gporcel49@gmail.com>
2025-01-17 10:49:53 -0300

    ajusta coluna O dados

--- app/Controller/Component/ExcelTemplate.php
+++ app/Controller/Component/ExcelTemplate.php
@@ -1977,7 +1977,7 @@ class ExcelTemplate
 				->setCellValue('L'. $indx, $dados[$i]['Supplier']['id'])
 				->setCellValue('M'. $indx, $dados[$i]['Supplier']['nome_fantasia'])
 				->setCellValue('N'. $indx, $dados[$i]['Benefit']['code'].'/'.$dados[$i]['Benefit']['name'])
-				->setCellValue('O'. $indx, $dados[$i]['CustomerUserItinerary']['price_per_day'])
+				->setCellValue('O'. $indx, $dados[$i]['CustomerUserItinerary']['unit_price'])
 				->setCellValue('P'. $indx, $dados[$i]['CustomerUserItinerary']['quantity'])
 				->setCellValue('Q'. $indx, $dados[$i]['OrderItem']['subtotal'])
 				->setCellValue('R'. $indx, $dados[$i]['OrderItem']['transfer_fee'])
