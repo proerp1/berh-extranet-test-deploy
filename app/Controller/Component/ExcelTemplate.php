c83468acde17a68b0ac3d8b6f83758908c1a0bc2
Gabriel Henrique Ruiz Porcel <gporcel49@gmail.com>
2025-04-17 10:21:22 -0300

    inclui campos em execel fornecedores

--- app/Controller/Component/ExcelTemplate.php
+++ app/Controller/Component/ExcelTemplate.php
@@ -746,6 +746,8 @@ class ExcelTemplate
         $sheet->setCellValue($col.'1', "Região");$col++; 
         $sheet->setCellValue($col.'1', "Observação");$col++; 
         $sheet->setCellValue($col.'1', "Realiza GE");$col++; 
+        $sheet->setCellValue($col.'1', "Quantidade");$col++; 
+        $sheet->setCellValue($col.'1', "Unidade de Tempo");$col++; 
 
         foreach ($dados_sup as $key => $dado) {
 
@@ -812,6 +814,9 @@ class ExcelTemplate
             $sheet->setCellValue('AR' . ($key + 2), $nomeRegiao);
             $sheet->setCellValue('AS' . ($key + 2), $dado['Supplier']['observacao']); $col++;
             $sheet->setCellValue('AT' . ($key + 2), $dado['Supplier']['realiza_gestao_eficiente'] ? 'Sim' : 'Não');
+            $sheet->setCellValue('AU' . ($key + 2), $dado['Supplier']['valor']);
+            $sheet->setCellValue('AV' . ($key + 2), $dado['Supplier']['unidade_tempo']);
+
         }
 
         $sheet2 = $objPHPExcel->createSheet();
