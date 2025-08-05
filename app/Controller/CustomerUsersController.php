a47499525c52afb748d81c33743843d91bc53dcd
Gabriel Henrique Ruiz Porcel <gporcel49@gmail.com>
2025-08-05 15:19:52 -0300

    ajusta excel incluindo endereco

--- app/Controller/CustomerUsersController.php
+++ app/Controller/CustomerUsersController.php
@@ -181,6 +181,14 @@ class CustomerUsersController extends AppController
                         'MaritalStatus.id = CustomerUser.marital_status_id'
                     ]
                 ],
+                [
+                    'table' => 'customer_addresses',
+                    'alias' => 'CustomerAddress',
+                    'type' => 'LEFT',
+                    'conditions' => [
+                        'CustomerAddress.customer_user_id = CustomerUser.id'
+                    ]
+                ],
                 // Add other joins as necessary
             ],
             'fields' => [
@@ -212,6 +220,8 @@ class CustomerUsersController extends AppController
                 'CostCenter.name',
                 'SalaryRange.range',
                 'MaritalStatus.status',
+                'CustomerAddress.address',
+
             ],
             'group' => ['CustomerUser.id', 'EconomicGroup.id']
         ]);
