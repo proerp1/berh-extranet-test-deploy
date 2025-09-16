d2718c959a98ce1059f93b6b416eea6c88a124b4
Caio Gralho <caio.dopradogralho@gmail.com>
2025-09-16 18:14:12 -0300

    feat: remove regra de códigos duplicados

--- app/Model/CustomerBenefitCode.php
+++ app/Model/CustomerBenefitCode.php
@@ -18,10 +18,6 @@ class CustomerBenefitCode extends AppModel
                 'message' => 'O benefício BE é obrigatório',
                 'last' => false,
             ],
-            'uniqueCombo' => array(
-                'rule' => array('checkUnique', array('benefit_id', 'customer_id')),
-                'message' => 'Esse código BE já está vinculado à outro código Cliente.'
-            )
         ],
         'code_customer' => [
             'required' => [
@@ -31,23 +27,6 @@ class CustomerBenefitCode extends AppModel
             ],
         ],
     ];
-
-    public function checkUnique($data, $fields) {
-        if (!is_array($fields)) {
-          $fields = array($fields);
-        }
-
-        $conditions = array();
-        foreach ($fields as $field) {
-          $conditions[$field] = $this->data[$this->alias][$field];
-        }
-
-        if (!empty($this->id)) {
-          $conditions[$this->alias . '.id !='] = $this->id;
-        }
-
-        return $this->isUnique($conditions, false);
-    }
 
     public function beforeFind($queryData)
     {
